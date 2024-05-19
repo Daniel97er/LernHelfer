f957ce8dd80aba28987507507b7beb6f720486eb
Daniel97er <sDaniel321@web.de>
2024-05-19 20:43:42 +0200

    update

--- app/src/main/java/com/example/lernhelfer/AppleActivity.kt
+++ app/src/main/java/com/example/lernhelfer/AppleActivity.kt
@@ -2,7 +2,6 @@ package com.example.lernhelfer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.lernhelfer.R
 
 class AppleActivity : AppCompatActivity() {
 
