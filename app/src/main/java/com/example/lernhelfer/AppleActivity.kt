1b0ffa3a18079786ffaf3ca302a1a9a5e8e48e6f
Daniel97er <sDaniel321@web.de>
2024-05-19 20:24:27 +0200

    update

--- app/src/main/java/com/example/lernhelfer/AppleActivity.kt
+++ app/src/main/java/com/example/lernhelfer/AppleActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mylistviewdemo
+package com.example.lernhelfer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/example/lernhelfer/MainActivity.kt
+++ app/src/main/java/com/example/lernhelfer/MainActivity.kt
@@ -1,13 +1,11 @@
-package com.example.mylistviewdemo
+package com.example.lernhelfer
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.lernhelfer.R
 
 class MainActivity : AppCompatActivity() {
 
