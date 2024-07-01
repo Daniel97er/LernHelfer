eb21b8e45084306430053f054c8ae89868dbbaa3
Daniel97er <sDaniel321@web.de>
2024-07-01 18:30:28 +0200

    update

--- app/src/main/java/com/example/lernhelfer/TausenderpreisActivity.kt
+++ app/src/main/java/com/example/lernhelfer/TausenderpreisActivity.kt
@@ -10,7 +10,7 @@ class TausenderpreisActivity : AppCompatActivity() {
     private lateinit var enterWerbepreis: EditText
     private lateinit var enterBruttoreichweite: EditText
     private lateinit var calculateButton: Button
-    private lateinit var textViewResult: TextViewwvej
+    private lateinit var textViewResult: TextViewwve
 
 
 
