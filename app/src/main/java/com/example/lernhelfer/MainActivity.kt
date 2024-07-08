802d638adf7e6b89124e76c5148eab6208dad1e1
Daniel97er <sDaniel321@web.de>
2024-07-08 19:59:20 +0200

    update

--- app/src/main/java/com/example/lernhelfer/MainActivity.kt
+++ app/src/main/java/com/example/lernhelfer/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         val listView1: ListView = findViewById(R.id.listview1)
         val listView2: ListView = findViewById(R.id.listview2)
 
-        val list1 = listOf("Bezugspreiskalkulation", "Deckungsbeitrag", "Preiselastizitaet", "Tausenderpreis", "Tausenderpreis(Qualitativ)")
+        val list1 = listOf("Bezugspreiskalkulation", "Deckungsbeitrag", "Preiselastizitaet", "Tausenderpreis", "Tausenderpreis(Qualitativ)", "Preiselastizitaett")
         val list2 = listOf("anne", "paul", "peter")
 
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list1)
@@ -34,6 +34,7 @@ class MainActivity : AppCompatActivity() {
                 2 -> startActivity(Intent(this@MainActivity, PreiselastizitaetActivity::class.java))
                 3 -> startActivity(Intent(this@MainActivity, TausenderpreisActivity::class.java))
                 4 -> startActivity(Intent(this@MainActivity, Tausenderpreis_Qualitativ_Activity::class.java))
+                5 -> startActivity(Intent(this@MainActivity, PreiselastizitaettActivity::class.java))
                 // Add more cases for other positions if needed
             }
         }
