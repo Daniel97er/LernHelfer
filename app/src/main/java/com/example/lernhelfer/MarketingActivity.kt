562925aac67db4995792efebbdabe371e7ed8465
Daniel97er <sDaniel321@web.de>
2024-08-13 22:18:59 +0200

    update

---
+++ app/src/main/java/com/example/lernhelfer/MarketingActivity.kt
@@ -0,0 +1,13 @@
+package com.example.lernhelfer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MarketingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_marketing)
+
+    }
+}
